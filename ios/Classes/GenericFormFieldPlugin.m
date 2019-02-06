c164e5004d4a3d2147ce73e98766d59fe802d975
John <johnmoscoso911@gmail.com>
2019-02-06 09:07:40 -0500

    without swift

---
+++ ios/Classes/GenericFormFieldPlugin.m
@@ -0,0 +1,20 @@
+#import "GenericFormFieldPlugin.h"
+
+@implementation GenericFormFieldPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"generic_form_field"
+            binaryMessenger:[registrar messenger]];
+  GenericFormFieldPlugin* instance = [[GenericFormFieldPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
